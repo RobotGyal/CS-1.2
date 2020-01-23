75cf87c14b56efa37f6039a400cb8c8c53b797dd
student <student@users-MacBook-Pro.local>
2020-01-23 11:11:02 -0800

    fixed spacing

--- Tweet-Generator/dictionary_words.py
+++ Tweet-Generator/dictionary_words.py
@@ -8,7 +8,8 @@ dictionary_words = words.readlines()
 rand_amount = randint(1, 12)
 
 #contains '\n' which is causing for sentence to be returned in new lines
-random_dictionary_words = sample(dictionary_words, rand_amount) 
+random_dictionary_words = sample(dictionary_words, rand_amount)
+random_dictionary_words = [word.rstrip('\n') for word in random_dictionary_words] 
 
-sentence = ''.join(random_dictionary_words)
+sentence = ' '.join(random_dictionary_words)
 print(sentence)
