41c4a9fa551936bdc51be1a35775613155f68e2e
student <student@users-MacBook-Pro.local>
2020-01-23 11:06:13 -0800

    random amount of words in a sentence

---
+++ Tweet-Generator/dictionary_words.py
@@ -0,0 +1,14 @@
+# IMPORTS
+from random import randint, sample
+
+words = open("/usr/share/dict/words", 'r')
+
+dictionary_words = words.readlines()
+
+rand_amount = randint(1, 12)
+
+#contains '\n' which is causing for sentence to be returned in new lines
+random_dictionary_words = sample(dictionary_words, rand_amount) 
+
+sentence = ''.join(random_dictionary_words)
+print(sentence)
