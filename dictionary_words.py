21cb998cd92a27218f22069c76a42930754dfbb2
student <student@users-MacBook-Pro.local>
2020-01-30 14:06:34 -0800

    removing excess

--- dictionary_words.py
+++ dictionary_words.py
@@ -1,14 +1,12 @@
 # IMPORTS
 from random import randint, sample
-from flask import request
-import app
+
 
 words = open("/usr/share/dict/words", 'r')
 
 dictionary_words = words.readlines()
 
 def pick_words(dictionary_words, num):
-    # rand_amount = randint(1, 12)
     random_dictionary_words = sample(dictionary_words, num)
     random_dictionary_words = [word.rstrip('\n') for word in random_dictionary_words] 
     sentence = ' '.join(random_dictionary_words)
